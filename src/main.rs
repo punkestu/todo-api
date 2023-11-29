b413dfe979cd797d14a77f5fa7f6d66d5cb8e870
Dobberman <pangestubima89@gmail.com>
2023-11-29 11:17:25 +0700

    hello actix

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(hello)
+    })
+        .bind(("0.0.0.0", 8000))?
+        .run()
+        .await
+}
